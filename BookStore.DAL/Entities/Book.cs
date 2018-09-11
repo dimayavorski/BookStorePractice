5ea5622c4e30dad9bfffec27e8a1363a2caea825
dmitriy <dimonslayerrooock@mail.ru>
2018-09-11 11:56:49 +0300

    test

--- BookStore.DAL/Entities/Book.cs
+++ BookStore.DAL/Entities/Book.cs
@@ -9,6 +9,6 @@
         public int AuthorId { get; set; }
         public Author Author { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category Category { get; set; }///
     }
 }
